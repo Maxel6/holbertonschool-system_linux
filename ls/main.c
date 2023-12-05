500a22419fd90e4b806a8880fa27d48412d3e786
Axel Malka <axelmalka@outlook.fr>
2023-12-05 09:57:22 +0000

    print without . & ..

--- ls/main.c
+++ ls/main.c
@@ -5,20 +5,29 @@
 #include <string.h>
 
 #define MAX_NAME_LENGTH 256
+/**
+ * main - entry point of the program
+ * Return: 0 on success
+ */
+int main(void)
+{
+	DIR *dir;
+	struct dirent *read;
 
-int main (int argc, char *argv[]){
-    DIR *dir;
-    struct dirent *read;
+	dir = opendir("..");
+	if (dir == NULL)
+	{
+		perror("Error opening directory");
+		exit(EXIT_FAILURE);
+	}
 
-    dir = opendir("..");
-    if (dir == NULL) {
-        perror("Error opening directory");
-        exit(EXIT_FAILURE);
-    }
-
-    while ((read = readdir(dir)) != NULL)
-        printf("%s  ", read->d_name);
-
-    closedir(dir);
-    return 0;
+	while ((read = readdir(dir)) != NULL)
+	{
+		if (strcmp(read->d_name, ".") != 0 && strcmp(read->d_name, "..") != 0)
+		{
+			printf("%s\n", read->d_name);
+		}
+	}
+	closedir(dir);
+	return (0);
 }
