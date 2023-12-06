764c55c3d530eaac94152290616a495806783fa4
Axel Malka <axelmalka@outlook.fr>
2023-12-06 11:35:09 +0000

    refacto &handle file in arg

--- ls/main.c
+++ ls/main.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-
-#define MAX_NAME_LENGTH 256
+#include <sys/stat.h>
+#include <unistd.h>
 
 /**
  * _strcmp - compare two strings
@@ -15,17 +15,99 @@
  */
 int _strcmp(const char *s1, const char *s2)
 {
-    while (*s1 != '\0' && *s2 != '\0')
-    {
-        if (*s1 != *s2)
-        {
-            return (*s1 - *s2);
-        }
-        s1++;
-        s2++;
-    }
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
 
-    return (*s1 - *s2);
+	return (*s1 - *s2);
+}
+int is_file(char *dir_name)
+{
+	struct stat sb;
+	int res = 0;
+
+	if ((lstat(dir_name, &sb) != -1) && S_ISREG(sb.st_mode))
+		res = 1;
+	return (res);
+}
+int print_file(int argc, char ***argv)
+{
+	struct stat sb;
+	int is_error = 0;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if ((lstat((*argv)[i], &sb) != -1) && S_ISREG(sb.st_mode))
+		{
+			printf("%s\n", (*argv)[i]);
+		}
+	}
+	return (is_error);
+}
+int is_dir(char *dir_name)
+{
+	struct stat sb;
+	int res = 0;
+
+	if ((lstat(dir_name, &sb) != -1) && S_ISDIR(sb.st_mode))
+		res = 1;
+	return (res);
+}
+int print_dir(int argc, char *program_name, char *dir_name)
+{
+	DIR *dir;
+	struct dirent *read;
+	int is_error;
+
+	dir = opendir(dir_name);
+	if (errno == EACCES)
+	{
+		fprintf(stderr, "%s: cannot open %s: ", program_name, dir_name);
+		perror("");
+		is_error = 1;
+	}
+	else
+	{
+		if (argc > 2)
+			printf("%s:\n", dir_name);
+		while ((read = readdir(dir)) != NULL)
+		{
+			if (_strcmp(read->d_name, ".") != 0 && _strcmp(read->d_name, "..")
+			!= 0 && read->d_name[0] != '.')
+			{
+				printf("%s  ", read->d_name);
+			}
+		}
+		printf("\n");
+	}
+	return (is_error);
+}
+
+int process_args(int argc, char ***argv)
+{
+	struct stat sb;
+	int i = 0;
+	int err = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (lstat((*argv)[i], &sb) == -1)
+		{
+			fprintf(stderr, "%s: cannot open directory %s: ", *argv[0], (*argv)[i]);
+			perror("");
+			(*argv)[i] = NULL;
+			err = 1;
+		}
+	}
+
+	return (err);
 }
 
 /**
@@ -36,72 +118,40 @@ int _strcmp(const char *s1, const char *s2)
  */
 int main(int argc, char *argv[])
 {
-    DIR *dir;
-    struct dirent *read;
-    int i = 0;
-    char *dir_name = ".";
-    if (argc <= 2)
-    {
-        if (argc == 2)
-        {
-            dir_name = argv[1];
-        }
-        dir = opendir(dir_name);
-        if (errno == EACCES)
-        {
-            fprintf(stderr, "%s: cannot open directory %s: ", argv[0], dir_name);
-            perror("");
-            exit(EXIT_FAILURE);
-        }
-        if (errno == ENOENT)
-        {
-            fprintf(stderr, "%s: cannot access %s: ", argv[0], dir_name);
-            perror("");
-            exit(EXIT_FAILURE);
-        }
-        while ((read = readdir(dir)) != NULL)
-        {
-            if (_strcmp(read->d_name, ".") != 0 && _strcmp(read->d_name, "..") != 0 && read->d_name[0] != '.')
-            {
-                printf("%s  ", read->d_name);
-            }
-        }
-        printf("\n");
+	int i = 0;
+	char *dir_name = ".";
+	int is_error = 0;
 
-        closedir(dir);
-    }
-    else
-    {
-        for (i = 1; i < argc; i++)
-        {
-            dir_name = argv[i];
+	is_error += process_args(argc, &argv);
+	if (argc <= 2)
+	{
+		if (argc == 2)
+		{
+			dir_name = argv[1];
+		}
 
-            dir = opendir(dir_name);
-            if (errno == EACCES)
-            {
-                fprintf(stderr, "%s: cannot open %s: ", argv[0], dir_name);
-                perror("");
-                exit(EXIT_FAILURE);
-            }
-            if (errno == ENOENT)
-            {
-                fprintf(stderr, "%s: cannot access %s: ", argv[0], dir_name);
-                perror("");
-
-                exit(EXIT_FAILURE);
-            }
-            printf("%s:\n", dir_name);
-            while ((read = readdir(dir)) != NULL)
-            {
-                if (_strcmp(read->d_name, ".") != 0 && _strcmp(read->d_name, "..") != 0 && read->d_name[0] != '.')
-                {
-                    printf("%s  ", read->d_name);
-                }
-            }
-            printf("\n\n");
-
-            closedir(dir);
-        }
-    }
-    return (0);
+		if (is_dir(dir_name))
+			is_error += print_dir(argc, argv[0], dir_name);
+		if (is_file(dir_name))
+		{
+			printf("%s\n", dir_name);
+		}
+	}
+	else
+	{
+		is_error += print_file(argc, &argv);
+		for (i = 1; i < argc; i++)
+		{
+			dir_name = argv[i];
+			if (dir_name)
+			{
+				if (is_dir(dir_name))
+					is_error += print_dir(argc, argv[0], dir_name);
+				printf("\n");
+			}
+		}
+	}
+	if (is_error)
+		exit(EXIT_FAILURE);
+	return (0);
 }
