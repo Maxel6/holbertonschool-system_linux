fe964923129d577e5644dd5e3fdaf70fb7d85d7d
Axel Malka <axelmalka@outlook.fr>
2023-12-06 13:27:47 +0000

    good code :)

--- ls/main.c
+++ ls/main.c
@@ -33,7 +33,7 @@ int is_file(char *dir_name)
 	struct stat sb;
 	int res = 0;
 
-	if ((*argv)[i] != NULL && (lstat(dir_name, &sb) != -1) && S_ISREG(sb.st_mode))
+	if (dir_name && (lstat(dir_name, &sb) != -1) && S_ISREG(sb.st_mode))
 		res = 1;
 	return (res);
 }
